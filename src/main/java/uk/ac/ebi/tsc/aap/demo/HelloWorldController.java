268a9cdb269e78b329386fe52acc9542234a60a9
Neil Goodgame <neilg@ebi.ac.uk>
2017-05-04 14:27:41 +0100

    Spring example MVC app with security

---
+++ src/main/java/uk/ac/ebi/tsc/aap/demo/HelloWorldController.java
@@ -0,0 +1,46 @@
+package uk.ac.ebi.tsc.aap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by neilg on 03/05/2017.
+ */
+
+@Controller
+@SpringBootApplication
+public class HelloWorldController {
+
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    @ResponseBody
+    public String welcomePage() {
+        return "Wellcome";
+    }
+
+
+    @RequestMapping(value = { "/hello**" }, method = RequestMethod.GET)
+    @ResponseBody
+    public String hello() {
+        return "Hello";
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    @ResponseBody
+    public String adminPage() {
+        return "Admin";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication( HelloWorldController.class);
+        springApplication.run(args);
+    }
+
+}
---
+++ src/test/java/uk/ac/ebi/tsc/aap/demo/HelloWorldControllerTest.java
@@ -0,0 +1,79 @@
+package uk.ac.ebi.tsc.aap.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by neilg on 04/05/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HelloWorldController.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class HelloWorldControllerTest {
+
+    @LocalServerPort
+    private int port;
+    private String rootUri;
+
+    public static String DEFAULT_USER = "username";
+    public static String DEFAULT_ADMIN = "admin";
+    public static String DEFAULT_PASSWORD = "password";
+
+    Map<String, String> getHeaders = new HashMap<>();
+
+    @Before
+    public void setup() throws URISyntaxException {
+        rootUri = "http://localhost:" + port + "";
+    }
+
+
+    @Test
+    public void welcomePage() throws Exception {
+        HttpResponse response = Unirest.get(rootUri)
+                .headers(createStandardGetHeaders())
+                .asString();
+        assertThat(response.getStatus(), is(equalTo(HttpStatus.OK.value())));
+    }
+
+    @Test
+    public void hello() throws Exception {
+        HttpResponse response = Unirest.get(rootUri + "/hello")
+                .headers(createStandardGetHeaders())
+                .asString();
+        assertThat(response.getStatus(), is(equalTo(HttpStatus.OK.value())));
+    }
+
+    @Test
+    public void adminPage() throws Exception {
+        HttpResponse response = Unirest.get(rootUri + "/admin")
+                .headers(createStandardGetHeaders())
+                .asString();
+        assertThat(response.getStatus(), is(equalTo(HttpStatus.OK.value())));
+    }
+
+    public static Map<String, String> createStandardGetHeaders() {
+        Map<String, String> h = new HashMap<>();
+        h.put("Authorization", "Basic " + Base64Coder.encodeString(DEFAULT_ADMIN + ":" + DEFAULT_PASSWORD));
+        return h;
+    }
+
+}
